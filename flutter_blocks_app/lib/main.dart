57959fc80bf8c56d403a1485b8a5db211f2ea8c8
ikdewaraja <imandidewaraja281@gmail.com>
2025-03-23 13:50:02 +0530

    homepage1

--- flutter_blocks_app/lib/main.dart
+++ flutter_blocks_app/lib/main.dart
@@ -11,13 +11,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(appBar: AppBar(
+      
+      home: Scaffold(
+        
+        
+        appBar: AppBar(
         backgroundColor: Color(0xFF9E00FF),
-        leading: Icon(Icons.menu,
+        leading: Icon(Icons.menu, size: 30.0,
          color: Colors.white,),
         title: Text( 'Flutter Blocks App',
-         style: TextStyle(color: Colors.white),),
+         style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700),
+         ),
       ),
+
+
+
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -26,9 +34,62 @@ class MyApp extends StatelessWidget {
                     style: TextStyle(fontSize: 20.0, color: Colors.black),
             ),
 
-               Text("",
-                    style: TextStyle(fontSize: 20.0, color: Colors.black),
-            ),
+       Container(
+        child: Container(
+
+          
+        ),
+       ),
+       
+             
+
+             Row(
+              children: [
+
+               Container(
+                child: Text(""),
+
+
+
+
+               ),
+
+
+               Container(
+
+
+
+               ),
+
+              ]
+             
+             ),
+
+             Container(
+
+
+             ),
+             Container(
+
+
+             ),
+
+
+             Row(
+              children: [
+                    Container(
+
+              ),
+
+
+              Container(
+
+
+              ),
+
+              ]
+          
+             ),
         
           ]
         ),
