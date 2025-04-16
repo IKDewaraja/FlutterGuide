b91fe4f4e3ff9a59e763f0489a3bce51b17db893
ikdewaraja <imandidewaraja281@gmail.com>
2025-04-16 12:17:11 +0530

    home page-colors, import user full name

---
+++ workout_planner/lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kSubtitleColor = Colors.grey;
+const kMainWhiteColor = Colors.white;
+const kMainColor = Color(0XFF0099FF);
+const kMainBlackColor = Color(0XFF333333);
+const kMainDarkBlue = Color(0XFF0147FC);
+const kMainPinkColor = Color(0xffFF0000);
+const kMainLightBlueColor = Color(0xff00f0ff);
+const kMainPurpleColor = Color(0xffeb00ff);
+const kSubPinkColor = Color(0xffff0068);
+
+
--- workout_planner/lib/pages/home_page.dart
+++ workout_planner/lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:workout_planner/constants/colors.dart';
+import 'package:workout_planner/constants/responsive.dart';
+import 'package:workout_planner/data/user_data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,27 +12,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Date and Time Formatters
   final DateFormat formatter = DateFormat('EEEE,MMMM');
   final DateFormat dayFormat = DateFormat('dd');
+
+  //User Data
+  final userData = user;
+
+
+
   @override
   Widget build(BuildContext context) {
-
     DateTime now = DateTime.now();
     String formattedDate = formatter.format(now);
-    String formattedDay =dayFormat.format(now);
+    String formattedDay = dayFormat.format(now);
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(" $formattedDate, $formattedDay"
-              ),
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  " $formattedDate, $formattedDay",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: kSubtitleColor,
+                  ),
+                ),
 
-            ],
-
-          
-          )
+                Text(
+                  userData.fullName,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                    color: kMainBlackColor,
+                  ),
+                ),
+              ],
+            ),
           ),
+        ),
       ),
     );
   }
