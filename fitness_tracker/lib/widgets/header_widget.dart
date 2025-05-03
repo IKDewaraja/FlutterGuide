f7d1a541da04dd8071bddf1047105c8a632761cd
ikdewaraja <imandidewaraja281@gmail.com>
2025-05-03 12:38:19 +0530

    dashboard- search  input field

--- fitness_tracker/lib/widgets/header_widget.dart
+++ fitness_tracker/lib/widgets/header_widget.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_tracker/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HeaderWidget extends StatelessWidget {
@@ -5,6 +6,55 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            style: const TextStyle(
+              color: greyColor,
+              ),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: cardBgColor,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.transparent)
+                ),
+                labelStyle: TextStyle(
+                  color: greyColor,
+
+                
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+               
+                  ),
+             
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 5
+                  ),
+                     hintText: "Search",
+                     hintStyle: TextStyle(
+                      color: greyColor,
+                
+                ),
+
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: greyColor,
+                        size: 21,
+                      ),
+                    ),
+                  ),
+        
+      ),
+      ],
+      );
+            
+  }}
